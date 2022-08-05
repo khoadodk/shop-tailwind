dce02d89350e0e046d8fa9d8b03cc559c5878553
unknown <khoado.dk@gmail.com>
2022-08-04 21:57:07 -0400

    Added cart drawer

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,15 @@
 // Put your application javascript here
+let ShopJS = {
+    updateQuantity(line, qty){
+        fetch('/cart/change.js', {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ quantity: qty, line: line})
+        })
+        .then(res => res.json())
+        .then(() => location.reload())
+        .catch(err=> console.error('Error: ', err))
+    }
+}
+
+window.ShopJS = ShopJS
